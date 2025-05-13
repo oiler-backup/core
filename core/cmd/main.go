24c4c544cbda54391029012bf6c9145835c47e00
Anton Shadrin <a.shadrin@yadro.com>
2025-05-13 18:29:37 +0300

    Update core to collect metrics

--- core/cmd/main.go
+++ core/cmd/main.go
@@ -17,12 +17,19 @@ limitations under the License.
 package main
 
 import (
+	context "context"
 	"crypto/tls"
 	"flag"
+	"log"
+	"net"
 	"os"
+
+	pb "github.com/AntonShadrinNN/oiler-backup/internal/metrics"
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
+	grpc "google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -37,6 +44,8 @@ import (
 
 	backupv1 "github.com/AntonShadrinNN/oiler-backup/api/v1"
 	"github.com/AntonShadrinNN/oiler-backup/internal/controller"
+	"github.com/prometheus/client_golang/prometheus"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -45,18 +54,68 @@ var (
 	setupLog = ctrl.Log.WithName("setup")
 )
 
+var (
+	successfulBackups = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "backup_requests_successful_total",
+			Help: "Total number of successful backup requests",
+		},
+	)
+
+	failedBackups = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "backup_requests_failed_total",
+			Help: "Total number of failed backup requests",
+		},
+	)
+)
+
+// Server implements the BackupService gRPC service
+type server struct {
+	pb.UnimplementedBackupMetricsServiceServer
+}
+
+// ReportSuccessfulBackup handles incoming gRPC requests
+func (s *server) ReportSuccessfulBackup(ctx context.Context, req *pb.BackupMetrics) (*emptypb.Empty, error) {
+	log.Printf("Received successful backup report: %s at %d", req.BackupName, req.Timestamp)
+
+	if req.Success {
+		successfulBackups.Inc()
+	} else {
+		failedBackups.Inc()
+	}
+
+	return &emptypb.Empty{}, nil
+}
+
+func startGRPCServer() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterBackupMetricsServiceServer(s, &server{})
+	log.Println("gRPC server is running on port 50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
+
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(backupv1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
+
+	metrics.Registry.MustRegister(successfulBackups, failedBackups)
+
 }
 
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
-	var secureMetrics bool
+	var secureMetrics bool = false
 	var enableHTTP2 bool
 	var tlsOpts []func(*tls.Config)
 	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
@@ -65,8 +124,8 @@ func main() {
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	flag.BoolVar(&secureMetrics, "metrics-secure", true,
-		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
+	// flag.BoolVar(&secureMetrics, "metrics-secure", true,
+	// 	"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 	opts := zap.Options{
@@ -76,7 +135,7 @@ func main() {
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
+	go startGRPCServer()
 	// if the enable-http2 flag is false (the default), http/2 should be disabled
 	// due to its vulnerabilities. More specifically, disabling http/2 will
 	// prevent from being vulnerable to the HTTP/2 Stream Cancellation and
