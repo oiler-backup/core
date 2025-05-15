672c5200c965c71c2e76673682eeddf8b21c5935
Anton Shadrin <a.shadrin@yadro.com>
2025-05-15 19:51:43 +0300

    Refactor mysql

--- mysql-adapter/backuper/go.mod
+++ mysql-adapter/backuper/go.mod
@@ -3,18 +3,17 @@ module mysql_backuper
 go 1.24.2
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.36.3
-	github.com/aws/aws-sdk-go-v2/config v1.29.14
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.79.2
+	github.com/AntonShadrinNN/oiler-backup-base v0.0.0-20250515161620-a280e085eab2
+	github.com/caarlos0/env/v11 v11.3.1
 	github.com/go-sql-driver/mysql v1.9.2
-	github.com/golang/protobuf v1.5.4
-	google.golang.org/grpc v1.72.0
-	google.golang.org/protobuf v1.36.6
+	go.uber.org/zap v1.27.0
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.36.3 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.10 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.29.14 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.67 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
@@ -22,15 +21,20 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.34 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.7.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.15 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.18.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.79.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.25.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.19 // indirect
 	github.com/aws/smithy-go v1.22.2 // indirect
-	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/grpc v1.72.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
---
+++ mysql-adapter/scheduler/internal/server/server.go
@@ -0,0 +1,149 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	pb "github.com/AntonShadrinNN/oiler-backup-base/proto"
+	serversbase "github.com/AntonShadrinNN/oiler-backup-base/servers/backup"
+	eg "github.com/AntonShadrinNN/oiler-backup-base/servers/backup/envgetters"
+)
+
+type ErrBackupServer = error
+
+type BackupServer struct {
+	pb.UnimplementedBackupServiceServer
+	kubeClient    *kubernetes.Clientset
+	jobsCreator   serversbase.JobsCreator
+	namespace     string
+	backuperImage string
+	restorerImage string
+	coreAddr      string
+	jobsStub      serversbase.JobsStub
+}
+
+func NewBackupServer(systemNamespace, backuperImg, restorerImg string) (*BackupServer, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load Kubernetes config: %w", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Kubernetes client: %w", err)
+	}
+
+	jobsCreator := serversbase.NewJobsCreator(clientset)
+	jobsStub := serversbase.NewJobsStub(
+		"mysql",
+		systemNamespace,
+		backuperImg,
+		restorerImg,
+	)
+	return &BackupServer{
+		kubeClient:    clientset,
+		jobsCreator:   jobsCreator,
+		namespace:     systemNamespace,
+		backuperImage: backuperImg,
+		restorerImage: restorerImg,
+		jobsStub:      jobsStub,
+	}, nil
+}
+
+func RegisterBackupServer(grpcServer *grpc.Server, systemNamespace, backuperImage, restorerImage string) error {
+	server, err := NewBackupServer(systemNamespace, backuperImage, restorerImage)
+	if err != nil {
+		return err
+	}
+	pb.RegisterBackupServiceServer(grpcServer, server)
+
+	return nil
+}
+
+func (s *BackupServer) Backup(ctx context.Context, req *pb.BackupRequest) (*pb.BackupResponse, error) {
+	cj := s.jobsStub.BuildBackuperCj(
+		req.Schedule,
+		eg.NewEnvGetterMerger([]eg.EnvGetter{
+			eg.CommonEnvGetter{
+				DbUri:        req.DbUri,
+				DbPort:       fmt.Sprint(req.DbPort),
+				DbUser:       req.DbUser,
+				DbPass:       req.DbPass,
+				DbName:       req.DbName,
+				S3Endpoint:   req.S3Endpoint,
+				S3AccessKey:  req.S3AccessKey,
+				S3SecretKey:  req.S3SecretKey,
+				S3BucketName: req.S3BucketName,
+				CoreAddr:     req.CoreAddr,
+			},
+			eg.BackuperEnvGetter{
+				MaxBackupCount: int(req.MaxBackupCount),
+			},
+		}),
+	)
+	name, namespace, err := s.jobsCreator.CreateCronJob(ctx, cj)
+	if errors.Is(err, serversbase.ErrAlreadyExists) {
+		return &pb.BackupResponse{
+			Status:           "Exists",
+			CronjobName:      name,
+			CronjobNamespace: namespace,
+		}, nil
+	}
+	if err != nil {
+		log.Printf("Failed to create CronJob: %v", err)
+		return &pb.BackupResponse{Status: "Failed to create CronJob"}, nil
+	}
+
+	return &pb.BackupResponse{
+		Status:           "CronJob created successfully",
+		CronjobName:      name,
+		CronjobNamespace: namespace,
+	}, nil
+}
+
+func (s *BackupServer) Restore(ctx context.Context, req *pb.BackupRestore) (*pb.BackupRestoreResponse, error) {
+	job := s.jobsStub.BuildRestorerJob(
+		eg.NewEnvGetterMerger([]eg.EnvGetter{
+			eg.CommonEnvGetter{
+				DbUri:        req.DbUri,
+				DbPort:       fmt.Sprint(req.DbPort),
+				DbUser:       req.DbUser,
+				DbPass:       req.DbPass,
+				DbName:       req.DbName,
+				S3Endpoint:   req.S3Endpoint,
+				S3AccessKey:  req.S3AccessKey,
+				S3SecretKey:  req.S3SecretKey,
+				S3BucketName: req.S3BucketName,
+				// CoreAddr:     req.CoreAddr, // TODO
+			},
+			eg.RestorerEnvGetter{
+				BackupRevision: req.BackupRevision,
+			},
+		},
+		),
+	)
+	name, namespace, err := s.jobsCreator.CreateJob(ctx, job)
+	if errors.Is(err, serversbase.ErrAlreadyExists) {
+		return &pb.BackupRestoreResponse{
+			Status:       "Exists",
+			JobName:      name,
+			JobNamespace: namespace,
+		}, nil
+	}
+	if err != nil {
+		log.Printf("Failed to create Job: %v", err)
+		return &pb.BackupRestoreResponse{Status: "Failed to create Job"}, nil
+	}
+
+	return &pb.BackupRestoreResponse{
+		Status:       "Job created successfully",
+		JobName:      name,
+		JobNamespace: namespace,
+	}, nil
+}
